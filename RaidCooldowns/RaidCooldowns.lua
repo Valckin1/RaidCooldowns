a00f3a985602cb8d9bdf3c8097a288d88b8b9c79
Valszone <valszone11875@gmail.com>
2026-04-21 18:24:21 -0500

    Bump main addon interface version to 120005

--- RaidCooldowns/RaidCooldowns.lua
+++ RaidCooldowns/RaidCooldowns.lua
@@ -302,7 +302,7 @@ RC._lastDragKey     = nil      -- prevents UpdateLayout spam
 RC.barPool = RC.barPool or {}   -- key -> bar frame
 
 RC.debugShowAllSpells = false
-RC.version = "0.3.1"
+RC.version = "0.3.2"
 
 ------------------------------------------------
 -- APPLY PANEL SIZE FROM SETTINGS 
